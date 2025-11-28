833901f3643a13970b0e93054c5dcc0b5a46663e
Cristian Dutkercz <37801730+Dutkercz@users.noreply.github.com>
2025-11-28 14:36:51 +0000

    Projeto SpringBoot iniciado dentro da subpasta backend

---
+++ backend/reciclaalegre/src/main/java/db/start/reciclaalegre/ReciclaalegreApplication.java
@@ -0,0 +1,13 @@
+package db.start.reciclaalegre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclaalegreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclaalegreApplication.class, args);
+	}
+
+}
---
+++ backend/reciclaalegre/src/test/java/db/start/reciclaalegre/ReciclaalegreApplicationTests.java
@@ -0,0 +1,13 @@
+package db.start.reciclaalegre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclaalegreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
