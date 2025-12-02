e9804a2a479d1cae7aa520bdd97c209dbbe54ac0
Dutkercz <dutkercz@gmail.com>
2025-12-02 18:48:59 -0300

    Repository de Solicitacao

---
+++ backend/reciclaalegre/src/main/java/db/start/reciclaalegre/repository/SolicitacaoRepository.java
@@ -0,0 +1,9 @@
+package db.start.reciclaalegre.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import db.start.reciclaalegre.model.Solicitacao;
+
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
+
+}
