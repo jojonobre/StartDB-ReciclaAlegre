861cf90df6f6d46a16b948497c65a709ea0df2d1
Matheus Patricio <matheus.bah3@gmail.com>
2025-12-03 12:01:44 -0300

    Consertando bugs das dependências e alterando os dto e mapper para ficarem configurados corretamente

--- backend/reciclaalegre/src/main/java/db/start/reciclaalegre/dto/UsuarioResponseDTO.java
+++ backend/reciclaalegre/src/main/java/db/start/reciclaalegre/dto/UsuarioResponseDTO.java
@@ -1,10 +1,13 @@
 package db.start.reciclaalegre.dto;
 
+import db.start.reciclaalegre.model.enums.TipoUsuario;
+
 public record UsuarioResponseDTO(
     Long id, 
     String nome, 
     String email, 
     String telefone, 
-    Boolean isAtivo) {
+    Boolean isAtivo,
+    TipoUsuario tipoUsuario) {
 
 }
--- backend/reciclaalegre/src/main/java/db/start/reciclaalegre/utils/mapper/UsuarioMapper.java
+++ backend/reciclaalegre/src/main/java/db/start/reciclaalegre/utils/mapper/UsuarioMapper.java
@@ -1,6 +1,7 @@
 package db.start.reciclaalegre.utils.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import db.start.reciclaalegre.dto.UsuarioRequestDTO;
 import db.start.reciclaalegre.dto.UsuarioResponseDTO;
@@ -11,5 +12,8 @@ public interface UsuarioMapper {
 
     UsuarioResponseDTO toDto(Usuario usuario);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "isAtivo", expression = "java(true)")
+    @Mapping(target = "tipoUsuario", ignore = true)
     Usuario toEntity(UsuarioRequestDTO dto);
 }
