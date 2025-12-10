142dcc20867ff79422711063d05dac4f0c5645e4
joycemaria <joycemaria.work@gmail.com>
2025-12-10 16:07:04 -0300

    Adicionando endpoint de atualizaçao de Soliticacao

--- backend/reciclaalegre/src/main/java/db/start/reciclaalegre/controller/SolicitacaoRestController.java
+++ backend/reciclaalegre/src/main/java/db/start/reciclaalegre/controller/SolicitacaoRestController.java
@@ -17,6 +17,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -39,20 +40,27 @@ public class SolicitacaoRestController {
     }
 
     @GetMapping
-    public ResponseEntity<List<SolicitacaoResponseDTO>> careggarSolicitacoes() {
+    public ResponseEntity<List<SolicitacaoResponseDTO>> carregarSolicitacoes() {
         List<SolicitacaoResponseDTO> solicitacoesDto = solicitacaoService.carregarAtivas();
         return ResponseEntity.ok(solicitacoesDto);
     }
 
-    @GetMapping("/by-usuario")
-    public ResponseEntity<List<SolicitacaoResponseDTO>> getMethodName(@AuthenticationPrincipal Usuario usuario) {
+    @GetMapping("/usuario")
+    public ResponseEntity<List<SolicitacaoResponseDTO>> carregarSolicitacoesPorUsuario(@AuthenticationPrincipal Usuario usuario) {
         return ResponseEntity.ok(solicitacaoService.carregarPorUsuario(usuario.getEmail()));
     }
 
-    @PostMapping("/claim/{id}")
+    @PostMapping("/requisitar/{id}")
     public ResponseEntity<?> requisitarColeta(@AuthenticationPrincipal Usuario usuario, @PathVariable Long id) {
         System.out.println("SOLICITAR COLETA " + id);
         return ResponseEntity.ok(solicitacaoService.requisitarColeta(usuario.getEmail(), id));
+    }
+
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<SolicitacaoResponseDTO> atualizarSolicitacao(@PathVariable Long id,
+            @RequestBody SolicitacaoRequestDTO dto, @AuthenticationPrincipal Usuario usuario) {
+        SolicitacaoResponseDTO atualizado = solicitacaoService.atualizarSolicitacao(id, dto, usuario.getEmail());
+        return ResponseEntity.ok(atualizado);
     }
 
 }
